638b851193c24e1f5ae96735ffd521058bd56102
Freddy Akpoly <fakpoly@u.rochester.edu>
2023-03-26 21:18:08 -0400

    update player

--- include/headers/player.h
+++ include/headers/player.h
@@ -1,43 +1,12 @@
-#pragma once
-#include <iostream>
-#include <sdl/SDL.h>
-#include <SDL_image.h>
-#include <SDL_TTF.h>
+#include <headers/Characters.h>
 
-
-class Player{
-private:
-    SDL_Point m_Position;
-    SDL_Texture* m_Texture;
-    SDL_Rect m_SpriteRect;
-    bool m_LeftPressed;
-    bool m_RightPressed;
-    bool m_UpPressed;;
-    bool m_DownPressed;
-    float m_Speed;
-    SDL_Renderer* m_renderer;
+class Player : public Characters
+{
+    public:
+      Player(Properties props);
+        virtual void Draw();
+        virtual void Clean();
+        virtual void Update(float dt);
 
     
-   
-
-public:
-    
-    Player();
-    ~Player();
-    void newPlayer();
-    SDL_Rect getSpriteRect();
-    SDL_Texture* getTexture();
-    void moveLeft();
-    void moveRight();
-    void moveUp();
-    void moveDown();
-    void stopLeft();
-    void stopRight();
-    void stopUp();
-    void stopDown();
-    void update(float elapsedTime);
-    void setRenderer(SDL_Renderer* renderer);
-    
-    
-      
 };
--- src/player.cpp
+++ src/player.cpp
@@ -1,119 +1,21 @@
-#include <player.h>
-#include <iostream>
+#include <headers/Player.h>
 
-Player::Player()
-{
-   // How fast does player move?
-    m_Speed = 2;
-    m_LeftPressed = false;
-    m_RightPressed = false;
+Player::Player(){
 
-    m_UpPressed = false;
-    m_DownPressed = false;
 }
 
-void Player:: newPlayer(){
-    
-   
-    // Associate a texture with the sprite
-    m_Texture = IMG_LoadTexture(m_renderer, "sprites\\player2.jpg");
-   
-    if (!m_Texture)
-    {
-        std::cout << "Error: Failed to load texture\n";
-    }
-    m_SpriteRect.x = 100;
-    m_SpriteRect.y = 100;
-    SDL_QueryTexture(m_Texture, nullptr, nullptr, &m_SpriteRect.w, &m_SpriteRect.h);
+Player::Player(Properties props){
+
 }
 
-Player::~Player()
-{
-    SDL_DestroyTexture(m_Texture);
+void Player::Draw(){
+
 }
 
-// Make the private sprite available to the draw() function
-SDL_Rect Player::getSpriteRect()
-{
-    return m_SpriteRect;
+void Player::Clean(){
+
 }
 
-SDL_Texture* Player::getTexture()
-{
-   return m_Texture;
-  
-}
+void Player::Update(float dt){
 
-void Player::moveLeft()
-{
-    m_LeftPressed = true;
-}
-
-void Player::moveRight()
-{
-    m_RightPressed = true;
-}
-
-void Player::moveUp()
-{
-    
-    m_UpPressed = true;
-}
-
-void Player::moveDown()
-{
-    m_DownPressed = true;
-}
-
-void Player::stopLeft()
-{
-    m_LeftPressed = false;
-}
-
-void Player::stopRight()
-{
-    m_RightPressed = false;
-}
-
-void Player::stopUp()
-{
-    m_UpPressed = false;
-}
-
-void Player::stopDown()
-{
-    m_DownPressed = false;
-}
-
-// Move Player based on the input this frame,
-// the time elapsed, and the speed
-void Player::update(float elapsedTime)
-{
-    if (m_RightPressed)
-    {
-        m_SpriteRect.x += m_Speed ;
-    }
-
-    if (m_LeftPressed)
-    {
-        
-        m_SpriteRect.x -= m_Speed ;
-        
-    }
-
-    if (m_UpPressed)
-    {
-        m_SpriteRect.y -= m_Speed;
-    }
-
-    if (m_DownPressed)
-    {
-        m_SpriteRect.y += m_Speed;
-    }
-}
-
-void Player::setRenderer(SDL_Renderer* renderer)
-{
-  m_renderer = renderer; 
-  newPlayer();
 }
