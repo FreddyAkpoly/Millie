50fb662d2d3fd4fb30f1affa231141875bd965f8
Freddy Akpoly <fakpoly@u.rochester.edu>
2023-04-04 22:26:21 -0400

    Update TileLayer.cpp

--- src/TileLayer.cpp
+++ src/TileLayer.cpp
@@ -10,6 +10,12 @@ TileLayer::TileLayer(int tilesize, int rowcount, int colcount, TileMap tilemap, 
 
 void TileLayer::Render(){
 
+    for(unsigned int i =0; i < m_RowCount;i++){
+        for(unsigned int j = 0; j<m_ColCount;j++){
+            
+        }
+    }
+
 }
 
 void TileLayer::Update(){
